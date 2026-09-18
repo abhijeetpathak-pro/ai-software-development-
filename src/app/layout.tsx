b717b8a73aad869c970b062173f76084c9f47a94
Abhijeet Pathak <codewithakhil6@gmail.com>
2026-09-18 11:24:58 +0530

    Switch default theme background to white across the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -95,7 +95,7 @@ const organizationJsonLd = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <head>
         {/* Google Tag Manager */}
         <script
